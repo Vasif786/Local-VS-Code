d040d94cb703488e218e95f0762c3d37fe898fb6
Vasif786 <farjanabanurajpura@gmail.com>
2026-08-20 05:04:10 +0530

    noone

--- CodeApp/Managers/SimulatorExtension.swift
+++ CodeApp/Managers/SimulatorExtension.swift
@@ -1,31 +1,16 @@
 import SwiftUI
 
 class SimulatorExtension: CodeAppExtension {
-
     override func onInitialize(
         app: MainApp,
         contribution: CodeAppExtension.Contribution
     ) {
-
-        // Existing Simulator button
         contribution.toolBar.registerItem(
             item: ToolbarItem(
                 extenionID: "simulator.button",
                 icon: "iphone",
                 onClick: {
                     SimulatorManager.shared.showDevicePicker = true
-                },
-                shouldDisplay: { _ in true }
-            )
-        )
-
-        // NEW iPhone 13 button
-        contribution.toolBar.registerItem(
-            item: ToolbarItem(
-                extenionID: "iphone13.button",
-                icon: "iphone",
-                onClick: {
-                    SimulatorManager.shared.showIPhone13 = true
                 },
                 shouldDisplay: { _ in true }
             )
